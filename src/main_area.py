e61bb1fbf055584e20821ba5172f2eb00017c878
Benjamin Hohl <benjamin.hohl@schmid-elektronik.ch>
2020-03-29 19:29:11 +0200

    show title

--- src/main_area.py
+++ src/main_area.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 
 def create_main_area(data, canton, masks, gloves, sanitizer):
+    st.title('COVID19 Equipment Calculator')
+
+    # show dataframe filtered by canton
     st.write(data[data['abbreviation_canton_and_fl'] == canton])
 
     # this is just to show that we can use the variables
