1aa7da621fabfedc344be1404568581acceea63d
danielbarco <danielbarco@users.noreply.github.com>
2020-03-29 20:07:23 +0200

    adapted function prep_data

--- src/data_prep.py
+++ src/data_prep.py
@@ -11,19 +11,22 @@ def load_data():
     return data
 
 def pre_process(data):
-    '''create summary rows, transform col to lowercase, create prediciton column'''
+    '''create summary rows, 'ensure all col are lowercase, transform date to index, and change all data to numeric, add prediction column'''
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     DATE_COLUMN = 'date'
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     data.set_index('date', inplace=True)
+    for column in list(data.columns.values):
+        if re.search("ncumul|ninst|Total", column):
+            data[column] = pd.to_numeric(data[column])
     df_switzerland = data.sum(level=0)
     df_switzerland['abbreviation_canton_and_fl'] = 'CH'
     data = data.append(df_switzerland)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data['prediction'] = 0
-
+    data.sort_values(by=['date'], inplace = True)
     return data
 
 
@@ -37,21 +40,10 @@ def predict(data):
     return data
 
 
-def postprocess_data(data):
-    '''ensure all col are lowercase, transform date to index, and change all data to numeric, add prediction column'''
-
-    for column in list(data.columns.values):
-        if re.search("ncumul|ninst|Total", column):
-            data[column] = pd.to_numeric(data[column])
-
-    return data
-
-
 def prep_data():
     '''calls all above functions'''
     data = load_data()
     data = pre_process(data)
     data = predict(data)
-    data = postprocess_data(data)
 
     return data
