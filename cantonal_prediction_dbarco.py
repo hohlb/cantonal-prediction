53f53a64936fc0e15eec4232d9aefc583ccfc4e6
danielbarco <danielbarco@users.noreply.github.com>
2020-03-29 11:30:00 +0200

    data pulled

--- cantonal_prediction_dbarco.py
+++ cantonal_prediction_dbarco.py
@@ -1,23 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+import re
 
 
 # ATTENTION
 # this is for now just the uber_pickup tutorial, it will be replaced soon with our actual code
 
-st.title('Uber pickups in NYC')
+st.title('COVID19 Equipment Calculator')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN = 'date'
+resp = requests.get('https://covid19-rest.herokuapp.com/api/openzh/v1/all')
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.DataFrame(resp.json()['records'])
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    for column in list(data.columns.values):
+        if re.search("ncumul|ninst|Total", column):
+            data[column] = pd.to_numeric(data[column])
+    data.set_index('date', inplace = True)
+    #data.rename(columns={'ncumul_conf': 'confirmed', 'ncumul_deceased': 'deaths', 'abbreviation_canton_and_fl' : 'country'}, inplace=True)
     return data
 
 data_load_state = st.text('Loading data...')
@@ -28,13 +34,13 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+# st.subheader('Number of pickups by hour')
+# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+# st.bar_chart(hist_values)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+# # Some number in the range 0-23
+# hour_to_filter = st.slider('hour', 0, 23, 17)
+# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+# st.map(filtered_data)
