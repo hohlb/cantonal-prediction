bbfbf3663a85348f68f46d6d2f9b1167b026c016
Benjamin Hohl <benjamin.hohl@schmid-elektronik.ch>
2020-03-30 09:36:31 +0200

    move method

--- src/calc_equip.py
+++ src/calc_equip.py
@@ -9,3 +9,25 @@ def calc_equip(data, ratio_dict):
         data[key] = data['ncumul_conf'].apply(lambda x: value * x)
 
     return data
+
+
+def calculate_needed_equipment(data, canton, masks, gloves_pair, sanitizer):
+    # filter by canton
+    region_data = data[data['abbreviation_canton_and_fl'] == canton]
+
+    equip = {
+        'mask': masks,
+        'gloves_pair': gloves_pair,
+        'sanitizer': sanitizer
+    }
+
+    needed_equip = calc_equip(region_data, equip)
+
+    # show needed equipment for the newest time period
+    days = 7
+    recent_period = needed_equip.tail(days)
+    recent_period = recent_period[[
+        'mask', 'gloves_pair', 'sanitizer'
+    ]]
+
+    return recent_period
--- src/main_area.py
+++ src/main_area.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.calc_equip import calc_equip
+from src.calc_equip import calculate_needed_equipment
 
 
 def create_main_area(data, canton, masks, gloves_pair, sanitizer):
@@ -7,25 +7,3 @@ def create_main_area(data, canton, masks, gloves_pair, sanitizer):
 
     needed_equipment = calculate_needed_equipment(data, canton, masks, gloves_pair, sanitizer)
     st.write(needed_equipment)
-
-
-def calculate_needed_equipment(data, canton, masks, gloves_pair, sanitizer):
-    # filter by canton
-    region_data = data[data['abbreviation_canton_and_fl'] == canton]
-
-    equip = {
-        'mask': masks,
-        'gloves_pair': gloves_pair,
-        'sanitizer': sanitizer
-    }
-
-    needed_equip = calc_equip(region_data, equip)
-
-    # show needed equipment for the newest time period
-    days = 7
-    recent_period = needed_equip.tail(days)
-    recent_period = recent_period[[
-        'mask', 'gloves_pair', 'sanitizer'
-    ]]
-
-    return recent_period
