41abc2ea448c302769b6eca310b6fb13e8fa2e4e
Benjamin Hohl <benjamin.hohl@schmid-elektronik.ch>
2020-03-30 09:30:51 +0200

    change app name

--- src/main_area.py
+++ src/main_area.py
@@ -3,7 +3,7 @@ from src.calc_equip import calc_equip
 
 
 def create_main_area(data, canton, masks, gloves_pair, sanitizer):
-    st.title('COVID19 Equipment Calculator')
+    st.title('COVID19 Equipment Predictor')
 
     # filter by canton
     region_data = data[data['abbreviation_canton_and_fl'] == canton]
